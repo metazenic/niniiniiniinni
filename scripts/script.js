f889c83d38bf43e83b2a522e7bb78fa3e1f52520
metazenic <ncristaldo3597@gmail.com>
2021-07-24 00:32:10 -0300

    [FIX] a la mierda todo, lo hice con un prompt, después veo qué onda el form

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,29 @@
 window.onload = () => {
-    const formulario = document.forms.formTexto
-    
-    formulario.addEventListener("submit", e => {
-        e.preventDefault();
-        
-        const textoTransformar = formulario.texto.value
-        const respuesta = document.querySelector("#respuesta")
-        const img = document.querySelector("img")
-        
-        respuesta.innerHTML = cambiarPorI(textoTransformar)
-
-        img.src = "https://i.imgflip.com/2uag15.png?a451971"
-        img.alt = "mocking kid"
-    })
-}
+  llamarPrompt();
+};
 
 function esVocal(letra) {
-    const vocales = "aeiouáéíóú"
-    return vocales.includes(letra)
+  const vocales = "aeiouáéíóú";
+  return vocales.includes(letra);
 }
 
 function cambiarPorI(texto) {
-    let textoFinal = ""
-    for (let caracter of texto) {
-        if (esVocal(caracter)) {
-            caracter = "i"
-        }
-        textoFinal += caracter
+  let textoFinal = "";
+  for (let caracter of texto) {
+    if (esVocal(caracter)) {
+      caracter = "i";
     }
-    return textoFinal
+    textoFinal += caracter;
+  }
+  return textoFinal;
+}
+
+function llamarPrompt() {
+  const parrafo = document.querySelector("#respuesta");
+  const imagen = document.querySelector("img");
+  const textoIngresado = prompt("ingrisi ti tixti piri trinsfirmirli");
+
+  parrafo.innerHTML = cambiarPorI(textoIngresado);
+  imagen.src = "https://i.imgflip.com/2uag15.png?a451971";
+  imagen.alt = "mocking kid";
 }
